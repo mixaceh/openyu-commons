0b61b9dbffa16787d39122369ae48f355d5427c3
mixaceh <mixaceh@gmail.com>
2015-10-16 16:20:30 +0800

    updae pom

---
+++ openyu-commons-core.j/src/test/java/org/openyu/commons/spring/jdbc/BenchmarkJdbcTest.java
@@ -0,0 +1,344 @@
+package org.openyu.commons.spring.jdbc;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openyu.commons.junit.supporter.BenchmarkDatabaseTestSupporter;
+import org.openyu.commons.lang.ArrayHelper;
+import org.openyu.commons.lang.ByteHelper;
+import org.openyu.commons.thread.ThreadHelper;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BenchmarkJdbcTest extends BenchmarkDatabaseTestSupporter {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		applicationContext = new ClassPathXmlApplicationContext(new String[] { //
+				"applicationContext-init.xml", //
+				"org/openyu/commons/spring/jdbc/testContext-jdbc.xml",//
+
+		});
+		// ---------------------------------------------------
+		initialize();
+		// ---------------------------------------------------
+	}
+
+	public static class BeanTest extends BenchmarkJdbcTest {
+		@Test
+		public void dataSource() {
+			System.out.println(dataSource);
+			assertNotNull(dataSource);
+		}
+
+		@Test
+		public void jdbcTemplate() {
+			System.out.println(jdbcTemplate);
+			assertNotNull(jdbcTemplate);
+		}
+	}
+
+	// ---------------------------------------------------
+	// optimized
+	// ---------------------------------------------------
+	public static class OptimizedTest extends BenchmarkJdbcTest {
+
+		@Test
+		// insert: 10000 rows, 102400000 bytes / 29690 ms. = 3448.97 BYTES/MS,
+		// 3368.14 K/S, 3.29 MB/S
+
+		// 2015/10/09
+		// insert: 10000 rows, 102628000 bytes / 82989 ms. = 1236.65 BYTES/MS,
+		// 1207.66 K/S, 1.18 MB/S
+		public void insert() throws Exception {
+			final int NUM_OF_THREADS = 100;
+			final int NUM_OF_TIMES = 100;
+			final int LENGTH_OF_BYTES = 10 * 1024;// 10k
+			//
+			final AtomicLong timesCounter = new AtomicLong(0);
+			final AtomicLong byteCounter = new AtomicLong(0);
+			// seq
+			final AtomicLong seqCounter = new AtomicLong(1);
+			//
+			ExecutorService service = Executors.newFixedThreadPool(NUM_OF_THREADS);
+			long beg = System.currentTimeMillis();
+			for (int i = 0; i < NUM_OF_THREADS; i++) {
+				//
+				final String userId = "TEST_USER_" + i;
+				service.submit(new Runnable() {
+					//
+					public void run() {
+						try {
+							//
+							for (int i = 0; i < NUM_OF_TIMES; i++) {
+								byte[] buff = ByteHelper.randomByteArray(LENGTH_OF_BYTES);
+								try {
+									StringBuilder sql = new StringBuilder();
+									sql.append("insert into TEST_BENCHMARK (seq, id, info) ");
+									sql.append("values (:seq, :id, :info)");
+
+									long seq = seqCounter.getAndIncrement();
+									// 0_0
+									String newId = userId + "_" + i;
+									// params
+									Object[] params = new Object[] { seq, newId, new String(buff) };
+									int inserted = jdbcTemplate.update(sql.toString(), params);
+
+									System.out.println("I[" + userId + "] R[" + i + "], " + inserted);
+									//
+									if (inserted > 0) {
+										timesCounter.incrementAndGet();
+										byteCounter.addAndGet(ByteHelper.toByteArray(seq).length);
+										byteCounter.addAndGet(ByteHelper.toByteArray(newId).length);
+										byteCounter.addAndGet(buff.length);
+									}
+									//
+									ThreadHelper.sleep(50);
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								} finally {
+								}
+							}
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						} finally {
+						}
+						//
+						ThreadHelper.sleep(3 * 1000);
+					}
+				});
+				//
+				ThreadHelper.sleep(50);
+			}
+			service.shutdown();
+			service.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+			//
+			printResult(beg, byteCounter, timesCounter);
+		}
+
+		@Test
+		// 2015/10/16 pc
+		// 10000 rows, 183473056 bytes / 26693 ms. = 6873.45 BYTES/MS, 6712.36
+		// K/S, 6.56 MB/S
+		public void select() throws Exception {
+			final int NUM_OF_THREADS = 100;
+			final int NUM_OF_TIMES = 100;
+			final int LENGTH_OF_BYTES = 10 * 1024;// 10k
+			//
+			final AtomicLong timesCounter = new AtomicLong(0);
+			final AtomicLong byteCounter = new AtomicLong(0);
+			// seq
+			final AtomicLong seqCounter = new AtomicLong(1);
+			//
+			ExecutorService service = Executors.newFixedThreadPool(NUM_OF_THREADS);
+			long beg = System.currentTimeMillis();
+			for (int i = 0; i < NUM_OF_THREADS; i++) {
+				//
+				final String userId = "TEST_USER_" + i;
+				service.submit(new Runnable() {
+					//
+					public void run() {
+						try {
+							//
+							for (int i = 0; i < NUM_OF_TIMES; i++) {
+								byte[] buff = ByteHelper.randomByteArray(LENGTH_OF_BYTES);
+								try {
+									StringBuilder sql = new StringBuilder();
+									sql.append("select seq, id, info from TEST_BENCHMARK ");
+									sql.append("where seq=:seq");
+
+									long seq = seqCounter.getAndIncrement();
+									// params
+									Object[] params = new Object[] { seq };
+									List<TestBenchmark> list = jdbcTemplate.query(sql.toString(), params,
+											new TestBenchmarkRowMapper());
+									//
+									seq = 0;
+									String id = null;
+									String info = null;
+									if (list.size() > 0) {
+										TestBenchmark row = list.get(0);
+										seq = row.getSeq();
+										id = row.getId();
+										info = row.getInfo();
+									}
+									System.out.println("I[" + id + "] R[" + i + "], " + seq);
+									//
+									if (seq > 0) {
+										timesCounter.incrementAndGet();
+										byteCounter.addAndGet(ByteHelper.toByteArray(seq).length);
+										byteCounter.addAndGet(ByteHelper.toByteArray(id).length);
+										byteCounter.addAndGet(ByteHelper.toByteArray(info).length);
+									}
+									//
+									ThreadHelper.sleep(50);
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								} finally {
+								}
+							}
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						} finally {
+						}
+						//
+						ThreadHelper.sleep(3 * 1000);
+					}
+				});
+				//
+				ThreadHelper.sleep(50);
+			}
+			service.shutdown();
+			service.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+			//
+			printResult(beg, byteCounter, timesCounter);
+		}
+
+		@Test
+		// update: 10000 rows, 102400000 bytes / 34485 ms. = 2969.41 BYTES/MS,
+		// 2899.81 K/S, 2.83 MB/S
+
+		// 2015/10/09
+		// update: 10000 rows, 102400000 bytes / 124789 ms. = 820.59 BYTES/MS,
+		// 801.35 K/S, 0.78 MB/S
+		public void update() throws Exception {
+			final int NUM_OF_THREADS = 100;
+			final int NUM_OF_TIMES = 100;
+			final int LENGTH_OF_BYTES = 10 * 1024;// 10k
+			//
+			final AtomicLong timesCounter = new AtomicLong(0);
+			final AtomicLong byteCounter = new AtomicLong(0);
+			// seq
+			final AtomicLong seqCounter = new AtomicLong(1);
+			//
+			ExecutorService service = Executors.newFixedThreadPool(NUM_OF_THREADS);
+			long beg = System.currentTimeMillis();
+			for (int i = 0; i < NUM_OF_THREADS; i++) {
+				//
+				final String userId = "TEST_USER_" + i;
+				service.submit(new Runnable() {
+					//
+					public void run() {
+						try {
+							//
+							for (int i = 0; i < NUM_OF_TIMES; i++) {
+								byte[] buff = ByteHelper.randomByteArray(LENGTH_OF_BYTES);
+								byte[] prefix = ByteHelper.toByteArray("UPDATE_");
+								buff = ArrayHelper.add(prefix,
+										ByteHelper.getByteArray(buff, 0, buff.length - prefix.length));
+								try {
+									StringBuilder sql = new StringBuilder();
+									sql.append("update TEST_BENCHMARK set info=:info ");
+									sql.append("where seq=:seq");
+
+									long seq = seqCounter.getAndIncrement();
+									// params
+									Object[] params = new Object[] { new String(buff), seq };
+									int updated = jdbcTemplate.update(sql.toString(), params);
+
+									System.out.println("I[" + userId + "] R[" + i + "], " + updated);
+									//
+									if (updated > 0) {
+										timesCounter.incrementAndGet();
+										byteCounter.addAndGet(buff.length);
+									}
+									//
+									ThreadHelper.sleep(50);
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								} finally {
+								}
+							}
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						} finally {
+						}
+						//
+						ThreadHelper.sleep(3 * 1000);
+					}
+				});
+				//
+				ThreadHelper.sleep(50);
+			}
+			service.shutdown();
+			service.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+			//
+			printResult(beg, byteCounter, timesCounter);
+		}
+
+		@Test
+		// delete: 10000 rows, 102400000 bytes / 18315 ms. = 5591.05 BYTES/MS,
+		// 5460.01 K/S, 5.33 MB/S
+
+		// 2015/10/09
+		// delete: 10000 rows, 102400000 bytes / 26270 ms. = 3897.98 BYTES/MS,
+		// 3806.62 K/S, 3.72 MB/S
+		public void delete() throws Exception {
+			final int NUM_OF_THREADS = 100;
+			final int NUM_OF_TIMES = 100;
+			final int LENGTH_OF_BYTES = 10 * 1024;// 10k
+			//
+			final AtomicLong timesCounter = new AtomicLong(0);
+			final AtomicLong byteCounter = new AtomicLong(0);
+			// seq
+			final AtomicLong seqCounter = new AtomicLong(1);
+			//
+			ExecutorService service = Executors.newFixedThreadPool(NUM_OF_THREADS);
+			long beg = System.currentTimeMillis();
+			for (int i = 0; i < NUM_OF_THREADS; i++) {
+				//
+				final String userId = "TEST_USER_" + i;
+				service.submit(new Runnable() {
+					//
+					public void run() {
+						try {
+							//
+							for (int i = 0; i < NUM_OF_TIMES; i++) {
+								byte[] buff = ByteHelper.randomByteArray(LENGTH_OF_BYTES);
+								try {
+									StringBuilder sql = new StringBuilder();
+									sql.append("delete from TEST_BENCHMARK ");
+									sql.append("where seq=:seq");
+
+									long seq = seqCounter.getAndIncrement();
+									// params
+									Object[] params = new Object[] { seq };
+									int deleted = jdbcTemplate.update(sql.toString(), params);
+
+									System.out.println("I[" + userId + "] R[" + i + "], " + deleted);
+									//
+									if (deleted > 0) {
+										timesCounter.incrementAndGet();
+										byteCounter.addAndGet(buff.length);
+									}
+									//
+									ThreadHelper.sleep(50);
+								} catch (Exception ex) {
+									ex.printStackTrace();
+								} finally {
+								}
+							}
+						} catch (Exception ex) {
+							ex.printStackTrace();
+						} finally {
+						}
+						//
+						ThreadHelper.sleep(3 * 1000);
+					}
+				});
+				//
+				ThreadHelper.sleep(50);
+			}
+			service.shutdown();
+			service.awaitTermination(Long.MAX_VALUE, TimeUnit.MINUTES);
+			//
+			printResult(beg, byteCounter, timesCounter);
+		}
+	}
+}
