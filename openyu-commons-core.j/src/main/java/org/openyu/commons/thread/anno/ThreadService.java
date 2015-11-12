caa4f722b8baa5986c769a4a6a8a240e850f64d8
mixaceh <mixaceh@gmail.com>
2015-11-12 23:01:52 +0800

    add @ThreadService

---
+++ openyu-commons-core.j/src/main/java/org/openyu/commons/thread/anno/ThreadService.java
@@ -0,0 +1,17 @@
+package org.openyu.commons.thread.anno;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+@Target({ ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Autowired
+@Qualifier("threadService")
+public @interface ThreadService {
+
+}
