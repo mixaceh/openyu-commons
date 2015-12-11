5d42f7c9800a548d54b41a07bdbe115e17e6f8a2
mixaceh <mixaceh@gmail.com>
2015-12-12 01:01:55 +0800

    refactor doExecute()

--- openyu-commons-core.j/src/main/java/org/openyu/commons/job/BaseJob.java
+++ openyu-commons-core.j/src/main/java/org/openyu/commons/job/BaseJob.java
@@ -2,28 +2,4 @@ package org.openyu.commons.job;
 
 public interface BaseJob {
 
-	String KEY = BaseJob.class.getName();
-
-	/**
-	 * 取消
-	 * 
-	 * @return
-	 */
-	boolean isCancel();
-
-	void setCancel(boolean cancel);
-
-	/**
-	 * 偵錯
-	 * 
-	 * @return
-	 */
-	boolean isLogEnable();
-
-	void setLogEnable(boolean logEnable);
-
-	/**
-	 * 執行
-	 */
-	void execute();
 }
--- openyu-commons-core.j/src/main/java/org/openyu/commons/job/supporter/BaseJobSupporter.java
+++ openyu-commons-core.j/src/main/java/org/openyu/commons/job/supporter/BaseJobSupporter.java
@@ -1,110 +1,41 @@
 package org.openyu.commons.job.supporter;
 
-import java.lang.reflect.Method;
-
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 import org.springframework.scheduling.quartz.QuartzJobBean;
-import org.springframework.util.StopWatch;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.openyu.commons.job.BaseJob;
-import org.openyu.commons.lang.ClassHelper;
-import org.openyu.commons.lang.StringHelper;
 import org.openyu.commons.mark.Supporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public abstract class BaseJobSupporter extends QuartzJobBean implements
-		BaseJob, Supporter {
+public abstract class BaseJobSupporter extends QuartzJobBean implements BaseJob, Supporter {
 
-	private static transient final Logger LOGGER = LoggerFactory
-			.getLogger(BaseJobSupporter.class);
-
-	private boolean cancel;
-
-	private boolean logEnable;
+	private static transient final Logger LOGGER = LoggerFactory.getLogger(BaseJobSupporter.class);
 
 	public BaseJobSupporter() {
 	}
 
-	public boolean isCancel() {
-		return cancel;
-	}
-
-	public void setCancel(boolean cancel) {
-		this.cancel = cancel;
-	}
-
-	public boolean isLogEnable() {
-		return logEnable;
-	}
-
-	public void setLogEnable(boolean logEnable) {
-		this.logEnable = logEnable;
-	}
-
-	protected void executeInternal(JobExecutionContext jobExecutionContext)
-			throws JobExecutionException {
+	@Override
+	protected void executeInternal(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 		try {
-			execute();
-		} catch (Exception ex) {
-			ex.printStackTrace();
+			doExecute(jobExecutionContext);
+		} catch (JobExecutionException e) {
+			LOGGER.error(new StringBuilder("Exception encountered during executeInternal()").toString(), e);
+			throw e;
+		} catch (Exception e) {
+			LOGGER.error(new StringBuilder("Exception encountered during executeInternal()").toString(), e);
+			throw new JobExecutionException(e);
 		}
 	}
 
-	protected void log(Logger logger, Class<?> clazz, String methodName) {
-		log(logger, clazz, methodName, null);
-	}
-
-	// [thread-19] LogService int save(), in 356 mills.
-	protected void log(Logger logger, Class<?> clazz, String methodName,
-			StopWatch stopWatch) {
-		if (logEnable) {
-			StringBuilder buff = new StringBuilder();
-			// thrad id
-			buff.append("T[" + Thread.currentThread().getId() + "]");
-			// s
-			Method method = ClassHelper.getDeclaredMethod(clazz, methodName);
-			if (method != null) {
-				buff.append(method.getDeclaringClass().getSimpleName());
-				buff.append(" ");
-				buff.append(method.getReturnType().getSimpleName());
-				buff.append(" ");
-				buff.append(method.getName());
-				//
-				Class<?>[] paramTypes = ClassHelper.getMethodParameterTypesAndCache(
-						method.getDeclaringClass(), method);
-				if (paramTypes != null) {
-					if (paramTypes.length == 0) {
-						buff.append("()");
-					} else {
-						buff.append("(");
-						for (int i = 0; i < paramTypes.length; i++) {
-							buff.append(paramTypes[i].getSimpleName());
-							if (i < paramTypes.length - 1) {
-								buff.append(StringHelper.COMMA + " ");
-							} else {
-								buff.append("),");
-							}
-						}
-					}
-				}
-			}
-
-			//
-			if (stopWatch != null) {
-				buff.append(", execute at ");
-				buff.append(stopWatch.getTotalTimeMillis());
-				buff.append(" mills.");
-			}
-			//
-			logger.info(buff.toString());
-		}
-	}
+	/**
+	 * 內部執行
+	 */
+	protected abstract void doExecute(JobExecutionContext jobExecutionContext) throws Exception;
 
 	public String toString() {
-		return ToStringBuilder.reflectionToString(this,
-				ToStringStyle.MULTI_LINE_STYLE);
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
 	}
 }
