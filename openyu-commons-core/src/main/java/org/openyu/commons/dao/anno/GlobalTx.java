a28b2cab8a7dd263f65288595d9d1bd87272547c
mixaceh <mixaceh@gmail.com>
2016-08-03 10:48:38 +0800

    upgrade to jdk 1.8

--- openyu-commons-core/src/main/java/org/openyu/commons/dao/anno/GlobalTx.java
+++ openyu-commons-core/src/main/java/org/openyu/commons/dao/anno/GlobalTx.java
@@ -5,11 +5,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-@Transactional("globalTx")
+@Transactional(value = "globalTx", propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
 public @interface GlobalTx {
 
 }
--- openyu-commons-core/src/main/java/org/openyu/commons/dao/anno/LogTx.java
+++ openyu-commons-core/src/main/java/org/openyu/commons/dao/anno/LogTx.java
@@ -5,6 +5,8 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 /**
@@ -14,7 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
  */
 @Target({ ElementType.METHOD, ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
-@Transactional("logTx")
+@Transactional(value = "logTx", propagation = Propagation.REQUIRED, isolation = Isolation.READ_COMMITTED)
 public @interface LogTx {
 
 }
