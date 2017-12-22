6a40153587877e0009d9d7802bf71176198dbe82
Mix Aceh <mixaceh@gmail.com>
2017-12-22 13:11:30 +0800

    Update StackCacheFactoryImpl.java

--- openyu-commons-core/src/main/java/org/openyu/commons/commons/pool/impl/StackCacheFactoryImpl.java
+++ openyu-commons-core/src/main/java/org/openyu/commons/commons/pool/impl/StackCacheFactoryImpl.java
@@ -1,62 +1,85 @@
 package org.openyu.commons.commons.pool.impl;
 
-import org.apache.commons.pool.impl.StackObjectPoolFactory;
+import org.apache.commons.pool.impl.StackObjectPool;
+import org.openyu.commons.commons.pool.CacheFactory;
 import org.openyu.commons.commons.pool.PoolableCacheFactory;
 import org.openyu.commons.commons.pool.StackCacheFactory;
+import org.openyu.commons.commons.pool.ex.CacheException;
+import org.openyu.commons.commons.pool.supporter.CacheFactorySupporter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class StackCacheFactoryImpl<T> extends PoolingCacheFactory<T>
-		implements StackCacheFactory<T> {
+public class StackCacheFactoryImpl<T> extends CacheFactorySupporter<T> implements StackCacheFactory<T> {
 
-	private static final long serialVersionUID = -5019167084750351983L;
+	private static final long serialVersionUID = 2226617146581060878L;
 
-	private static final transient Logger LOGGER = LoggerFactory
-			.getLogger(StackCacheFactoryImpl.class);
+	private static final transient Logger LOGGER = LoggerFactory.getLogger(StackCacheFactoryImpl.class);
 
-	public StackCacheFactoryImpl(
-			PoolableCacheFactory<T> cacheableObjectFactory) {
-		super(cacheableObjectFactory);
+	/**
+	 * 物件池
+	 */
+	private StackObjectPool<T> objectPool;
+
+	public StackCacheFactoryImpl(PoolableCacheFactory<T> poolableCacheFactory) {
+		super(poolableCacheFactory);
 	}
 
 	public StackCacheFactoryImpl() {
+		this(null);
 	}
 
-//	/**
-//	 * new建構
-//	 * 
-//	 * remove to StackCacheFactoryFactoryBean.createService()
-//	 * 
-//	 * @return
-//	 */
-//	public static <T> StackCacheFactory<T> createInstance(
-//			CacheableObjectFactory<T> cacheableObjectFactory) {
-//		StackCacheFactoryImpl<T> result = null;
-//		try {
-//			result = new StackCacheFactoryImpl<T>(cacheableObjectFactory);
-//			result.setCreateInstance(true);
-//			// 啟動
-//			result.start();
-//		} catch (Exception e) {
-//			LOGGER.error(
-//					new StringBuilder().append(
-//							"Exception encountered during createInstance()")
-//							.toString(), e);
-//			result = (StackCacheFactoryImpl<T>) shutdownInstance(result);
-//		}
-//		return result;
-//	}
-
 	/**
-	 * 內部啟動
+	 * 建立工廠
+	 * 
+	 * @return
+	 * @throws CacheException
 	 */
 	@Override
-	protected void doStart() throws Exception {
-		super.doStart();
+	protected synchronized CacheFactory<T> createCacheFactory() throws CacheException {
+		if (this.instance != null) {
+			if (this.instance.isClosed()) {
+				throw new CacheException("PoolingCacheFactory was already closed");
+			}
+			return this.instance;
+		}
+		// 建立物件池
+		createObjectPool();
+		// 建立副本
+		createInstance();
 		//
-		// this.objectPool = new StackObjectPool<T>(cacheableObjectFactory);
-		this.objectPoolFactory = new StackObjectPoolFactory<T>(poolableCacheFactory);
-		this.objectPool = objectPoolFactory.createPool();
+		try {
+			// 檢驗可池化工廠
+			validatePoolableFactory();
+			// 初始大小
+			for (int i = 0; i < getInitialSize(); ++i) {
+				this.objectPool.addObject();
+			}
+		} catch (Exception e) {
+			throw new CacheException("Error preloading the pool", e);
+		}
+		//
+		LOGGER.info("Create CacheFactory");
+		return this.instance;
+	}
+
+	/**
+	 * 建立物件池
+	 */
+	@Override
+	protected void createObjectPool() {
+		// 設定factory
+		StackObjectPool<T> objectPool = new StackObjectPool<T>(getPoolableCacheFactory());
+		this.objectPool = objectPool;
+	}
+
+	/**
+	 * 建立副本
+	 * 
+	 * @throws CacheException
+	 */
+	@Override
+	protected void createInstance() throws CacheException {
+		this.instance = new PoolingCacheFactory<T>(this.objectPool);
 	}
 
 }
